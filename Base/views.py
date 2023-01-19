4b5825d91a522d2573f8dbe47a61c72c8225f061
Shagbaor Agber(Pyman) <67190277+Codewithshagbaor@users.noreply.github.com>
2023-01-19 03:42:14 +0100

    Add files via upload

---
+++ Base/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import requests
+
+def index(request):
+  if request.method == 'POST':
+    
+    song_title = request.POST.get('song_title')
+    artist = request.POST.get('artist')
+    
+    api_key = '3f6aed992212100966b2d06b87df667f'
+    api_url = f'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track={song_title}&apikey={api_key}&q_artist={artist}'
+    
+    response = requests.get(api_url)
+    data = response.json()
+    try:
+      lyrics = data['message']['body']['lyrics']['lyrics_body']
+      context = {
+        'lyrics':lyrics
+        }
+      return render(request, 'index.html', context)
+    except KeyError as e:
+      return render(request, 'error.html', {'error': f'{e} not found'})
+  else:
+    return render(request, 'index.html')
+  return render(request, 'index.html')
